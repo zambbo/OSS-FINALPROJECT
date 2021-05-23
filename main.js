458834d4521c9e5337d20fa29c2c6b4c54fb1153
Namul2 <qudwlskbj@naver.com>
2021-05-23 15:19:31 +0900

    modify window size, color, link botton

--- main.js
+++ main.js
@@ -1,17 +1,16 @@
-const {app,BrowserWindow} = require("electron");
+const { app, BrowserWindow } = require("electron");
 const path = require("path");
-
 
 let win;
 
 function createWindow() {
     win = new BrowserWindow({
-        width:800,
-        height:600,
+        width: 880,
+        height: 700,
         webPreferences: {
             nodeIntegration: true, // renderer process에서 node.js api 접근 가능.
             contextIsolation: false, // node.js api 쓰기위해서.
-            preload: path.join(__dirname,"preload.js"),
+            preload: path.join(__dirname, "preload.js"),
         },
     });
 
@@ -19,19 +18,18 @@ function createWindow() {
     win.loadFile("index.html");
 }
 
-app.whenReady().then(() =>{
+app.whenReady().then(() => {
     createWindow();
 
-    app.on("activate", () =>{
-        if(BrowserWindow.getAllWindows().length === 0){
+    app.on("activate", () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
             createWindow();
         }
     });
 });
 
-app.on("window-all-closed",() =>{
-    if(process.platform !== "darwin"){
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
-
